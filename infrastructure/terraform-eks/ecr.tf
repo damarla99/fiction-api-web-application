f9753dd0872bd65749d55986854016a25beb8621
sdamarla <sdamarla@adobe.com>
2025-12-04 22:02:57 -0600

    added ecr repo

--- infrastructure/terraform-eks/ecr.tf
+++ infrastructure/terraform-eks/ecr.tf
@@ -1,6 +1,6 @@
 # ECR Repository
 resource "aws_ecr_repository" "main" {
-  name                 = var.project_name
+  name                 = "${var.project_name}-${var.environment}"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
