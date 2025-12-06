b2525844970bc1acf45b86ceb280992f4f6235eb
sdamarla <sdamarla@adobe.com>
2025-12-05 18:19:14 -0600

    removed dynamodb and validated the terrafrom

--- infrastructure/terraform-eks/variables.tf
+++ infrastructure/terraform-eks/variables.tf
@@ -19,7 +19,7 @@ variable "project_name" {
 variable "cluster_version" {
   description = "Kubernetes version for EKS cluster"
   type        = string
-  default     = "1.34"  # Latest stable version (as of Dec 2024)
+  default     = "1.34" # Latest stable version (as of Dec 2024)
 }
 
 variable "vpc_cidr" {
